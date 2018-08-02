53f7a98aefe3f75a176a233e96360949fd8c40eb
koriim <ahmdkoriem@gmail.com>
2018-08-02 18:28:40 +0200

    adding app component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import React from 'react'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+    state = {
+        savedBooks: [] // books user has added to a shelf
+      }
+
+      componentDidMount() {
+        BooksAPI.getAll().then((books) => this.setState({
+          savedBooks: books
+        }))
+      }
+
+      moveBook = (book) => {
+        BooksAPI.update(book, book.shelf).then(() => {
+          var bookAlreadySaved = this.state.savedBooks.find((savedBook) => {
+            return savedBook.id === book.id;
+          })
+          bookAlreadySaved
+            ? this.updateExistingSavedBook(book)
+            : this.addNewSavedBook(book)
+        })
+      }
+
+      addNewSavedBook = (book) => {
+        this.setState((prevState) => ({
+          savedBooks: prevState.savedBooks.concat([book])
+        }))
+      }
+
+      updateExistingSavedBook = (book) => {
+        let savedBooks = this.state.savedBooks
+        const index = savedBooks.findIndex((savedBook) => (savedBook.id === book.id))
+        savedBooks[index].shelf = book.shelf
+        this.setState(() => ({
+          savedBooks: savedBooks
+        }))
+      }
 
   render() {
     return (
